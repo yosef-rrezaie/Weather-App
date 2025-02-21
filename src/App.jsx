e7d2d7a07fc3f8c02a43007a608c9f0adae779cb
yosef-rrezaie <yosefrrezaie@gmail.com>
2025-02-21 07:47:39 +0330

    create page and layout and config api

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import HomePage from "../Pages/HomePage";
+
 function App() {
-  return <></>;
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+        refetchOnMount: false,
+        staleTime: 60 * 1000,
+      },
+    },
+  });
+
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <HomePage />
+      </QueryClientProvider>
+    </>
+  );
 }
 
 export default App;
