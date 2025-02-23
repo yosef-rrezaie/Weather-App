800578072f029856632b763e04c26e2c6f4bd4f1
Yosef Rezaie <yosefrezaie5001@gmail.com>
2025-02-24 01:31:53 +0330

    change 24h to 12h and set am and pm for chart

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -15,9 +15,11 @@ function Chart({ weatherForcast }) {
   for (let i = 0; i < 8; i++) {
     let x = {};
     let key = new Date(weatherForcast["list"][i]["dt_txt"]).getHours();
+    let key_12h = key % 12 || 12
+    let key_AM_PM = key >= 12 ?  "بعد از ظهر" : "قبل از ظهر"
     let value = Math.floor(weatherForcast["list"][i]["main"]["temp"]);
     let value2 = Math.floor(weatherForcast["list"][i]["main"]["feels_like"]);
-    x["name"] = key;
+    x["name"] = key_12h  + key_AM_PM;
     x["دما"] = value;
     x["دمای احساسی"] = value2;
     data.push(x);
@@ -25,15 +27,15 @@ function Chart({ weatherForcast }) {
 
   console.log(data);
   return (
-    <div className="bg-white shadow-lg rounded-lg p-[25px]  h-[370px] flex-grow">
+    <div className="bg-white shadow-lg rounded-lg p-[25px]  h-[360px] flex-grow">
       <h2 className="text-xl font-semibold text-gray-700 mb-4">نمودار دما</h2>
       <ResponsiveContainer width="100%" height={300}>
         <LineChart data={data} margin={{ top: 20, right: 30, left: 40, bottom: 40 }}>
           <CartesianGrid strokeDasharray="4 4" stroke="#ddd" />
           <XAxis dataKey="name" tick={{ fontSize: 14 }} dy={10} />
           <YAxis tickFormatter={(value) => `${value}°C`} tick={{ fontSize: 14 }} dx={-35} />
-          <Tooltip formatter={(value) => `${value}°C`} />
-          <Line type="monotone" dataKey="دما" stroke="#3A80BA" strokeWidth={3} dot={{ r: 5 }} />
+          <Tooltip  formatter={(value) => `${value}°C`} />
+          <Line type="monotone" dataKey="دما" stroke="#3A80BA" strokeWidth={3} dot={{ r: 0 }} />
         </LineChart>
       </ResponsiveContainer>
     </div>
