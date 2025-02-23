ee7b41ef8959f4d32b38d7f2cbb1e289ecc1efe0
Yosef Rezaie <yosefrezaie5001@gmail.com>
2025-02-24 02:32:15 +0330

    add dateConverter

--- src/components/DetailWeather.jsx
+++ src/components/DetailWeather.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import {
   BsFillSunriseFill,
   BsFillSunsetFill,
-  BsSpeedometer2,
 } from "react-icons/bs";
 import { GiMultiDirections } from "react-icons/gi";
 import { IoIosSpeedometer } from "react-icons/io";
+import { dateConverter } from "../../config/dateConverter";
 
 function DetailWeather({ currentData }) {
   return (
@@ -15,13 +15,12 @@ function DetailWeather({ currentData }) {
         <div className="flex justify-between mt-[30px]">
           <div className="flex items-center">
             <span>
-              <BsFillSunriseFill className="text-[1.7rem] text-yellow-500" />
+              <BsFillSunriseFill className="text-[1.7rem] text-yellow-600" />
             </span>
             <div className="flex flex-col pr-[10px] gap-y-2">
-              <p className="">رطوبت</p>
+              <p className="">طلوع</p>
               <span className="font-thin">
-                {currentData["main"]["humidity"]}
-                <span>%</span>
+                {dateConverter( currentData["sys"]["sunrise"], "sunrise")} قبل از ظهر
               </span>
             </div>
           </div>
@@ -32,13 +31,12 @@ function DetailWeather({ currentData }) {
         <div className="flex justify-between mt-[30px]">
           <div className="flex items-center">
             <span>
-              <BsFillSunsetFill className="text-[1.7rem] text-yellow-500" />
+              <BsFillSunsetFill className="text-[1.7rem] text-blue-600" />
             </span>
             <div className="flex flex-col pr-[10px] gap-y-2">
-              <p className="">رطوبت</p>
+              <p className="">غروب</p>
               <span className="font-thin">
-                {currentData["main"]["humidity"]}
-                <span>%</span>
+                {dateConverter(currentData["sys"]["sunset"] , "sunset")} بعد از ظهر
               </span>
             </div>
           </div>
@@ -49,13 +47,12 @@ function DetailWeather({ currentData }) {
         <div className="flex justify-between mt-[30px]">
           <div className="flex items-center">
             <span>
-              <GiMultiDirections className="text-[1.7rem] text-yellow-500" />
+              <GiMultiDirections className="text-[1.7rem] text-green-800" />
             </span>
             <div className="flex flex-col pr-[10px] gap-y-2">
-              <p className="">رطوبت</p>
+              <p className="">جهت باد</p>
               <span className="font-thin">
-                {currentData["main"]["humidity"]}
-                <span>%</span>
+                {currentData["wind"]["deg"]}°
               </span>
             </div>
           </div>
@@ -66,13 +63,12 @@ function DetailWeather({ currentData }) {
         <div className="flex justify-between mt-[30px]">
           <div className="flex items-center">
             <span>
-              <IoIosSpeedometer className="text-[1.7rem] text-yellow-500" />
+              <IoIosSpeedometer className="text-[1.7rem] text-purple-500" />
             </span>
             <div className="flex flex-col pr-[10px] gap-y-2">
-              <p className="">رطوبت</p>
+              <p className="">فشار</p>
               <span className="font-thin">
-                {currentData["main"]["humidity"]}
-                <span>%</span>
+                {currentData["main"]["pressure"]} پاسکال
               </span>
             </div>
           </div>
