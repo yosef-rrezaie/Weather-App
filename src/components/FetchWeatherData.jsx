b1c515fc4a320f9129fead916d4584f4d7aa4212
Yosef Rezaie <yosefrezaie5001@gmail.com>
2025-02-26 01:44:39 +0330

    Better management of states with useContext

--- src/components/FetchWeatherData.jsx
+++ src/components/FetchWeatherData.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import { API_KEY, weatherApi } from "../config/api";
 import { useQuery } from "@tanstack/react-query";
 import ShowCurrentWeather from "./ShowCurrentWeather";
@@ -6,10 +6,16 @@ import { Chart } from "./Chart";
 import DetailWeather from "./DetailWeather";
 import ForcastWeather from "./ForcastWeather";
 import { IoIosRefreshCircle } from "react-icons/io";
+import { ComponentsContext } from "../Pages/HomePage";
 
-function FetchWeatherData({ data, cityName, language, units, setUnits }) {
-  console.log(data[0].lat, data[0].lon);
-  const { data: currentData, isPending: currentDataPending , refetch : currentRefetch } = useQuery({
+function FetchWeatherData() {
+  const { data, cityName, language, units, setUnits } =
+    useContext(ComponentsContext);
+  const {
+    data: currentData,
+    isPending: currentDataPending,
+    refetch: currentRefetch,
+  } = useQuery({
     queryKey: ["currentWeather", cityName, units],
     queryFn: () =>
       weatherApi.get(
@@ -17,7 +23,11 @@ function FetchWeatherData({ data, cityName, language, units, setUnits }) {
       ),
   });
 
-  const { data: weatherForcast, isPending: weatherForcastPending , refetch : forcastRefetch  } = useQuery({
+  const {
+    data: weatherForcast,
+    isPending: weatherForcastPending,
+    refetch: forcastRefetch,
+  } = useQuery({
     queryKey: ["weatherForcast", cityName, units],
     queryFn: () =>
       weatherApi.get(
@@ -25,12 +35,12 @@ function FetchWeatherData({ data, cityName, language, units, setUnits }) {
       ),
   });
 
-  console.log("current:", currentData);
-  console.log("forcast:", weatherForcast);
+  // console.log("current:", currentData);
+  // console.log("forcast:", weatherForcast);
 
-  function refetchHandler () {
-    currentRefetch()
-    forcastRefetch()
+  function refetchHandler() {
+    currentRefetch();
+    forcastRefetch();
   }
 
   return (
@@ -39,13 +49,14 @@ function FetchWeatherData({ data, cityName, language, units, setUnits }) {
         <div>
           <div className="flex justify-between mx-[43px] mt-[30px] ">
             <p className="text-[1.3rem] font-extrabold">موقعیت من</p>
-            <IoIosRefreshCircle className="text-blue-300 text-[2rem]" onClick={refetchHandler} />
+            <IoIosRefreshCircle
+              className="text-blue-300 text-[2rem]"
+              onClick={refetchHandler}
+            />
           </div>
           <div className="grid grid-cols-2 gap-5 mx-[43px] mb-[43px] mt-[25px]  min-h-[300px]">
             <ShowCurrentWeather
               currentData={currentData}
-              units={units}
-              setUnits={setUnits}
             />
             <Chart weatherForcast={weatherForcast} />
           </div>
