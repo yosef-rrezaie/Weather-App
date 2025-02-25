83f93c1d5aee00f99d1a92b819757eb33360e739
yosef-rrezaie <yosefrrezaie@gmail.com>
2025-02-26 02:27:38 +0330

    Search input control if the user enters the wrong city name

---
+++ src/components/SearchFailed.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { LuSearchX } from "react-icons/lu";
+
+function SearchFailed() {
+  return (
+    <div className=" flex h-[80vh] flex-col items-center justify-center">
+      <LuSearchX className="text-[10rem] text-red-300" />
+      <p className="font-bold mt-[8px]">
+        اطلاعات شهر مورد نظر موجود نمی باشد ، لطفا مجددا جست و جو نمایید
+      </p>
+    </div>
+  );
+}
+
+export default SearchFailed;
