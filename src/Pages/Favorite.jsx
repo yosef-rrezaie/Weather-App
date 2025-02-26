63a11477a9d1f2637b78398866821f5a4086b749
Yosef Rezaie <yosefrezaie5001@gmail.com>
2025-02-26 23:25:13 +0330

    Add noBookMark component for when no city is saved

--- src/Pages/Favorite.jsx
+++ src/Pages/Favorite.jsx
@@ -1,7 +1,11 @@
-import React, { useContext } from 'react'
+import React, { useContext } from "react";
+import { useSelector } from "react-redux";
+import NoBookMark from "../components/NoBookMark";
 
 function Favorite() {
-  
+  const result = useSelector((store) => store.Favorite.favoriteObject);
+  console.log(result);
+  return <>{!result.length? <NoBookMark/> : <p>bye</p>}</>;
 }
 
-export default Favorite
+export default Favorite;
