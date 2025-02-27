c539b9f34b87c256492d4f8b1c28b37da1bf9e69
Yosef Rezaie <yosefrezaie5001@gmail.com>
2025-02-27 04:44:45 +0330

    add SearchRecently reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import favortireReducer from "../feautures/favotire"
+import serachRecentlyReducer from "../feautures/serachRecently"
 
 const store = configureStore({
-    reducer : {Favorite : favortireReducer }
+    reducer : {Favorite : favortireReducer , SearchRecently : serachRecentlyReducer  }
 })
 
 export default store
