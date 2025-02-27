af36bd2a050e81f1cec93e46c58bc9144b6d907f
Yosef Rezaie <yosefrezaie5001@gmail.com>
2025-02-27 04:37:30 +0330

    Give navigate to notfound

--- src/Pages/PageNotFound.jsx
+++ src/Pages/PageNotFound.jsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import { TbError404 } from "react-icons/tb";
+import { ComponentsContext } from "../App";
+import { useNavigate } from "react-router-dom";
 
 function PageNotFound() {
+  const { cityName } = useContext(ComponentsContext);
+  let x = cityName.length;
+  const navigate = useNavigate();
+  // useEffect(() => {
+  //   if (cityName.length > x || cityName.length < x) {
+  //     navigate("/");
+  //   }
+  // }, [cityName]);
   return (
     <div className="flex justify-center items-center h-[80vh] flex-col">
       <TbError404 className=" text-blue-500 text-[10rem]" />
       <p>صفحه مورد نظر موجود نمی باشد !</p>
-      <button className="mt-[27px] rounded-[7px]  p-[10px] border-[.5px] 
-      border-solid border-blue-400 bg-blue-500 text-white shadow-2xl text-[12px]">
+      <button
+        className="mt-[27px] rounded-[7px]  p-[10px] border-[.5px] 
+      border-solid border-blue-400 bg-blue-500 text-white shadow-2xl text-[12px]"
+      onClick={()=> navigate("/")}>
         بازگشت به صفحه اصلی
-      </button>
+      </button >
     </div>
   );
 }
