a1b46bd6a34449054fa3963ae3ffe2d6d681ff71
Yosef Rezaie <yosefrezaie5001@gmail.com>
2025-02-27 03:38:57 +0330

    create 404(Page Not Found !)

--- src/Pages/PageNotFound.jsx
+++ src/Pages/PageNotFound.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { TbError404 } from "react-icons/tb";
 
 function PageNotFound() {
   return (
-    <div>PageNotFound</div>
-  )
+    <div className="flex justify-center items-center h-[80vh] flex-col">
+      <TbError404 className=" text-blue-500 text-[10rem]" />
+      <p>صفحه مورد نظر موجود نمی باشد !</p>
+      <button className="mt-[27px] rounded-[7px]  p-[10px] border-[.5px] 
+      border-solid border-blue-400 bg-blue-500 text-white shadow-2xl text-[12px]">
+        بازگشت به صفحه اصلی
+      </button>
+    </div>
+  );
 }
 
-export default PageNotFound
+export default PageNotFound;
