7b9053ddc45645984ed2949f8b3f13d6b8b7f256
Yosef Rezaie <yosefrezaie5001@gmail.com>
2025-02-27 08:47:43 +0330

    redirect to Homepage

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,15 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { API_KEY, geoApi } from "../config/api";
 import Layout from "../layout/Layout";
 import FetchWeatherData from "../components/FetchWeatherData";
 import SearchFailed from "../components/SearchFailed";
 import Loading from "../components/Loading";
-import { ComponentsContext } from "../App"; // ایمپورت کانتکست از App.js
+import { ComponentsContext } from "../App"; // 
 import { useSelector } from "react-redux";
 
 function HomePage() {
-  const { cityName, language } = useContext(ComponentsContext); // دریافت مقدار از کانتکست
+  const { cityName, language } = useContext(ComponentsContext); 
   const result = useSelector(store => store.Favorite.favoriteObject)
   console.log("result :" , result)
 
