6d0eaadcf31e757d54819ccf84fe7c838185edb0
Yosef Rezaie <yosefrezaie5001@gmail.com>
2025-02-27 10:27:16 +0330

    set darkmood for favoritepage

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -5,7 +5,7 @@ import { ComponentsContext } from "../App";
 function Layout({ children }) {
   const {dark , setDark} = useContext(ComponentsContext)
   return (
-    <div className={`bg-[#f1f0f0] ${dark && "darkmood-home"}`}>
+    <div className={`bg-[#f1f0f0] h-[100vh] ${dark && "darkmood-home text-white"}`}>
       <HeaderComponent />
       {children}
     </div>
