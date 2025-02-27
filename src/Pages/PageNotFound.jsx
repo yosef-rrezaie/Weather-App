b06b5cf911a3a957586e2fbbc83ebfe54ce94dd0
yosef-rrezaie <yosefrrezaie@gmail.com>
2025-02-27 12:03:48 +0330

    darkmood style finished

--- src/Pages/PageNotFound.jsx
+++ src/Pages/PageNotFound.jsx
@@ -4,7 +4,7 @@ import { ComponentsContext } from "../App";
 import { useNavigate } from "react-router-dom";
 
 function PageNotFound() {
-  const { cityName } = useContext(ComponentsContext);
+  const { cityName, dark } = useContext(ComponentsContext);
   let x = cityName.length;
   const navigate = useNavigate();
   // useEffect(() => {
@@ -13,15 +13,20 @@ function PageNotFound() {
   //   }
   // }, [cityName]);
   return (
-    <div className="flex justify-center items-center h-[80vh] flex-col">
-      <TbError404 className=" text-blue-500 text-[10rem]" />
+    <div className="flex justify-center items-center h-[80vh] flex-col ">
+      <TbError404
+        className={` text-blue-500 text-[10rem] ${dark && "text-blue"}`}
+      />
       <p>صفحه مورد نظر موجود نمی باشد !</p>
       <button
-        className="mt-[27px] rounded-[7px]  p-[10px] border-[.5px] 
-      border-solid border-blue-400 bg-blue-500 text-white shadow-2xl text-[12px]"
-      onClick={()=> navigate("/")}>
+        className={`mt-[27px] rounded-[7px]  p-[10px] border-[.5px] 
+      border-solid border-blue-400 bg-blue-500 text-white shadow-2xl text-[12px] ${
+        dark && "bg-[#00ADB5]"
+      }`}
+        onClick={() => navigate("/")}
+      >
         بازگشت به صفحه اصلی
-      </button >
+      </button>
     </div>
   );
 }
