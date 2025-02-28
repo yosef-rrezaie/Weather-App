184c13a218bf3da21b1fc7ff30dabbc8bd711431
yosef-rrezaie <yosefrrezaie@gmail.com>
2025-02-28 09:02:02 +0330

    respnosive search failed

--- src/components/ForcastWeather.jsx
+++ src/components/ForcastWeather.jsx
@@ -48,7 +48,7 @@ function ForcastWeather({ weatherForcast }) {
           key={item.index}
           className={`flex justify-around
             items-center rounded-[7px]  p-[10px] border-[.5px] border-solid border-gray-100 
-            shadow-sm ${dark && "border-[#272626]"}  `}
+            shadow-sm ${dark && "forcast-border"}  `}
         >
           <p className="w-3/4">
             {moment.unix(item["stamp"]).format("dddd") === "آدینه"
